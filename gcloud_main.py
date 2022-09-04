a392ed4e36e7406fe2f43abaf5420c6880d643d9
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-09-04 10:26:18 +0800

    Stable commit for gcloud

--- gcloud_main.py
+++ gcloud_main.py
@@ -1,6 +1,6 @@
 import os
 def main():
-    # os.system("python \"train.py\" --dataroot=\"E:/Image Transfer - Patches/\" --num_threads=6 --batch_size=512 --preprocess=crop --crop_size=32 --lambda_identity=0.0 --continue_train")
+    # os.system("python \"train.py\" --dataroot=\"/home/neil_delgallego/Image Transfer - Patches/\" --num_threads=8 --batch_size=256 --preprocess=crop --crop_size=32 --continue_train")
     os.system("python \"test.py\" --dataroot=\"E:/Image Transfer - Patches/\" --num_threads=12 --batch_size=512 --netG=resnet_9blocks --dataset_mode=unaligned")
 if __name__ == "__main__":
     main()
